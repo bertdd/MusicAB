1610a810954d7ac6cc1c7e06ed5740103794ebaf
Selcukk70 <106600043+Selcukk70@users.noreply.github.com>
2022-11-29 16:20:10 +0100

    Initials

---
+++ Person.Class.php
@@ -0,0 +1,36 @@
+<?php
+
+class Person
+{
+  public int $Id;
+
+  public string $FirstName;
+
+  public string $LastName;
+
+  public string $MiddleName;
+
+  public string $Initials;
+
+  public array $Email;
+
+  public array $Phone;
+
+  public string $Address;
+
+  public string $ZipCode;
+
+  public string $Place;
+
+  public string $Country;
+
+  //public DateTime $DateOfBirth;
+
+  //public DateTime $DateOfDeath;
+
+
+  
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-echo 'Goodbye cruel silly disgusting World!';
-// Akif was here
-// Kagu was here
+require_once ('Music.class.php');
+
+$person = new Artist("Bruce", "Springsteen");
+
+echo $person->Initials;
+
+
 ?>
